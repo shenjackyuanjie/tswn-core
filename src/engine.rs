c0521df3ba27fdb63e5e866b6ab8640abcad1c4c
shenjack <3695888@qq.com>
2024-06-20 20:33:40 +0800

    一些重构

--- src/engine.rs
+++ src/engine.rs
@@ -26,6 +26,9 @@ pub mod runners {
         /// 1: 错误原因
         #[error("PlayerGroup parse error: {0}")]
         PlayerGroupParseError(#[from] PlayerGroupError),
+        /// 某个人在创建过程中报错
+        #[error("Player parse error: {0}")]
+        PlayerError(#[from] PlayerError),
         /// 只有一个队伍
         #[error("Only one group")]
         OnlyOneGroup,
@@ -39,23 +42,7 @@ pub mod runners {
     }
 
     impl PlayerGroup {
-        /// 从一个 名竞的原始输入 中创建一个 PlayerGroup
-        ///
-        /// # 要求
-        /// 会默认整个输入是同一个队伍的
-        /// 也就是会忽略所有 \n\n 的队伍分割
-        pub fn new_from_namerena_raw(raw_input: String) -> PlayerGroupResult<PlayerGroup> {
-            // 首先以 \n 分割
-            let raw_input = raw_input.split("\n");
-            // 然后直接 map 生成 Player
-            let mut players = Vec::new();
-            for raw_name in raw_input {
-                let player = Player::new_from_namerena_raw(raw_name.to_string())?;
-                players.push(player);
-            }
-
-            Ok(PlayerGroup { players })
-        }
+        pub fn new(players: Vec<Player>) -> PlayerGroup { PlayerGroup { players } }
     }
 
     pub struct Runner {
@@ -74,31 +61,38 @@ pub mod runners {
         ///
         /// 其实就是解析名竞的输入格式
         pub fn new_from_namerena_raw(raw_input: String) -> RunnerResult<Runner> {
-            // 首先以 \n\n 分割
-            let mut raw_input = raw_input.split("\n\n");
-            let mut groups = Vec::new();
-            // 如果只有一组
-            if raw_input.clone().count() == 1 {
-                // 每个玩家一组
-                let input = raw_input.next().unwrap();
-                // 用 \n 分割
-                let raw_input = input.split("\n");
-                // 如果只有一个人
-                // 直接丢个错误
-                if raw_input.clone().count() == 1 {
-                    return Err(RunnerError::OnlyOneGroup);
-                }
-                for player in raw_input {
-                    let group = PlayerGroup::new_from_namerena_raw(player.to_string())?;
-                    groups.push(group);
-                }
-            } else {
-                // 每个组一组
-                for raw_group in raw_input {
-                    let group = PlayerGroup::new_from_namerena_raw(raw_group.to_string())?;
-                    groups.push(group);
-                }
+            let spilted_input = raw_input.split("\n");
+            let mut players = Vec::new();
+            for raw_player in spilted_input {
+                let player = Player::new_from_namerena_raw(raw_player.to_string())?;
+                players.push(player);
             }
+
+            // 首先以 \n\n 分割
+            // let mut raw_input = raw_input.split("\n\n");
+            // let mut groups = Vec::new();
+            // // 如果只有一组
+            // if raw_input.clone().count() == 1 {
+            //     // 每个玩家一组
+            //     let input = raw_input.next().unwrap();
+            //     // 用 \n 分割
+            //     let raw_input = input.split("\n");
+            //     // 如果只有一个人
+            //     // 直接丢个错误
+            //     if raw_input.clone().count() == 1 {
+            //         return Err(RunnerError::OnlyOneGroup);
+            //     }
+            //     for player in raw_input {
+            //         let group = PlayerGroup::new_from_namerena_raw(player.to_string())?;
+            //         groups.push(group);
+            //     }
+            // } else {
+            //     // 每个组一组
+            //     for raw_group in raw_input {
+            //         let group = PlayerGroup::new_from_namerena_raw(raw_group.to_string())?;
+            //         groups.push(group);
+            //     }
+            // }
             // // 尝试生成 PlayerGroup
             // let mut players = Vec::new();
             // for raw_group in raw_input {
@@ -111,3 +105,6 @@ pub mod runners {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {}
