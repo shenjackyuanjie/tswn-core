e51c810712fc2d40312b8b5493710e0d0de7448e
shenjack-5600u <3695888@qq.com>
2024-07-24 19:52:26 +0800

    绷

--- src/engine.rs
+++ src/engine.rs
@@ -80,7 +80,8 @@ pub mod runners {
         /// - 将 \r 替换成 \n
         /// - 将 大于等于3个 \n 替换成 2个 \n
         ///
-        pub fn spilt_namerena_into_groups(raw_input: String) -> Vec<Vec<String>> {
+        /// 返回: (队伍, seed)
+        pub fn spilt_namerena_into_groups(raw_input: String) -> (Vec<Vec<String>>, Vec<String>) {
             // 去除尾部的一个/多个 \n/带有几个空格的情况
             let raw_input = raw_input.trim_end();
             // 处理一下有 \r\n 的情况
@@ -91,10 +92,28 @@ pub mod runners {
             while raw_input.contains("\n\n\n") {
                 raw_input = raw_input.replace("\n\n\n", "\n\n");
             }
+            // 先把 SEED_PREFIX 取出来
+            let seed = raw_input
+                .split("\n")
+                .filter(|x| Player::check_is_seed(x))
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>();
             // 首先，如果没有\n\n, 那么一行就是一个队伍
             if !raw_input.contains("\n\n") {
-                return raw_input.split("\n").map(|x| vec![x.to_string()]).collect();
+                return (
+                    raw_input
+                        .split("\n")
+                        .filter(|x| !Player::check_is_seed(x))
+                        .map(|x| vec![x.to_string()])
+                        .collect(),
+                    seed,
+                );
             }
+            let raw_input = raw_input
+                .split("\n")
+                .filter(|x| !Player::check_is_seed(x))
+                .collect::<Vec<&str>>()
+                .join("\n");
             // 如果有\n\n, 那么就是一个队伍
 
             // 下面是为了修复一些容易手误的情况
@@ -111,7 +130,7 @@ pub mod runners {
 
             // 先检查有没有单独的seed玩家
             // let mut need_fix = false;
-            let mut groups = raw_input
+            let groups = raw_input
                 .split("\n\n")
                 .map(|x| x.split("\n").map(|x| x.to_string()).collect())
                 .collect::<Vec<Vec<String>>>();
@@ -134,7 +153,7 @@ pub mod runners {
                 }
             }
 
-            raw_input.split("\n\n").map(|x| x.split("\n").map(|x| x.to_string()).collect()).collect()
+            (raw_input.split("\n\n").map(|x| x.split("\n").map(|x| x.to_string()).collect()).collect(), seed)
         }
     }
 }
@@ -144,6 +163,37 @@ pub mod runners {
 mod group {
     use super::*;
 
+    macro_rules! str_vec {
+        () => {{
+            let vec: Vec<String> = Vec::with_capacity(0);
+            vec
+        }};
+    }
+
+    macro_rules! plr {
+        () => {
+            str_vec!()
+        };
+        ($($x:expr),+ $(,)?) => (
+            vec![
+                // 填充 x, 每一个都调用一遍 to_string
+                $($x.to_string()),+
+            ]
+        );
+    }
+
+    // 自动把每一个分开填
+    macro_rules! plrs {
+        () => {
+            str_vec!(str_vec!())
+        };
+        ($($x:expr),+ $(,)?) => (
+            vec![
+                $(plr!($x),)+
+            ]
+        );
+    }
+
     mod spilt_namerena_groups {
         use super::*;
         #[test]
@@ -151,16 +201,16 @@ mod group {
             // 没有 \n\n 的最基本情况
             let raw_input = "a\nb\nc".to_string();
             let groups = runners::Runner::spilt_namerena_into_groups(raw_input);
-            assert_eq!(groups, vec![vec!["a"], vec!["b"], vec!["c"]]);
+            assert_eq!(groups, (plrs!("a", "b", "c"), plr!()));
 
             // 跟随着一个或者多个尾部 \n 的情况
             // 自动忽略
             let raw_input = "a\nb\nc\n".to_string();
             let groups = runners::Runner::spilt_namerena_into_groups(raw_input);
-            assert_eq!(groups, vec![vec!["a"], vec!["b"], vec!["c"]]);
+            assert_eq!(groups, (plrs!("a", "b", "c"), plr!()));
             let raw_input = "a\nb\nc\n\n".to_string();
             let groups = runners::Runner::spilt_namerena_into_groups(raw_input);
-            assert_eq!(groups, vec![vec!["a"], vec!["b"], vec!["c"]]);
+            assert_eq!(groups, (plrs!("a", "b", "c"), plr!()));
         }
 
         #[test]
@@ -168,7 +218,7 @@ mod group {
             // 有 \n\n 的情况
             let raw_input = "a\nb\n\nc\nd".to_string();
             let groups = runners::Runner::spilt_namerena_into_groups(raw_input);
-            assert_eq!(groups, vec![vec!["a", "b"], vec!["c", "d"]]);
+            assert_eq!(groups, (vec![plr!["a", "b"], plr!["c", "d"]], plr!()));
         }
 
         #[test]
@@ -179,14 +229,14 @@ mod group {
                 let new_lines = "\n".repeat(x);
                 let raw_input = format!("a\nb{}c\nd", new_lines);
                 let groups = runners::Runner::spilt_namerena_into_groups(raw_input);
-                assert_eq!(groups, vec![vec!["a", "b"], vec!["c", "d"]]);
+                assert_eq!(groups, (vec![plr!["a", "b"], plr!["c", "d"]], plr!()));
             }
             // 以及有多个队伍的情况
             for x in 2..10 {
                 let new_lines = "\n".repeat(x);
                 let raw_input = format!("a\nb{}c\nd{}e", new_lines, new_lines);
                 let groups = runners::Runner::spilt_namerena_into_groups(raw_input);
-                assert_eq!(groups, vec![vec!["a", "b"], vec!["c", "d"], vec!["e"]]);
+                assert_eq!(groups, (vec![plr!["a", "b"], plr!["c", "d"], plr!["e"]], plr!()));
             }
         }
 
@@ -195,7 +245,7 @@ mod group {
             // 多个队伍
             let raw_input = "a\nb\nc\nd\ne\nf".to_string();
             let groups = runners::Runner::spilt_namerena_into_groups(raw_input);
-            assert_eq!(groups, vec![vec!["a"], vec!["b"], vec!["c"], vec!["d"], vec!["e"], vec!["f"]]);
+            assert_eq!(groups, (plrs!("a", "b", "c", "d", "e", "f"), plr!()));
         }
 
         #[test]
@@ -203,7 +253,7 @@ mod group {
             // 一个seed
             let raw_input = "seed: a@!\nb\nc".to_string();
             let groups = runners::Runner::spilt_namerena_into_groups(raw_input);
-            assert_eq!(groups, vec![vec!["seed: a@!"], vec!["b"], vec!["c"]]);
+            assert_eq!(groups, (plrs!("b", "c"), plr!["seed: a@!"]));
         }
 
         #[test]
@@ -213,7 +263,7 @@ mod group {
             let groups = runners::Runner::spilt_namerena_into_groups(raw_input);
             // assert_eq!(groups, vec![vec!["aaaa", "bbbb"], vec!["seed: a@!"]]);
             // 这个情况下，应该是修复成三个队伍
-            assert_eq!(groups, vec![vec!["aaaa"], vec!["bbbb"], vec!["seed: a@!"]])
+            assert_eq!(groups, (plrs!("aaaa", "bbbb"), plr!["seed: a@!"]))
         }
     }
 }
